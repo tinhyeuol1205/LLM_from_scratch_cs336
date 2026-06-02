787b9f987e7f78f47a6ef047d8f9218dd99b6296
binh.dv <nhathotinhtuyetvong.241201@gmail.com>
2026-06-02 12:17:40 +0700

    feat: parallelize data preprocessing using multiprocess chunking and boundary detection

--- cs336_basics/preprocess_data.py
+++ cs336_basics/preprocess_data.py
@@ -3,24 +3,30 @@ import typing
 from cs336_basics.tokenizer import Tokenizer
 import numpy as np
 from tqdm import tqdm
+from cs336_basics.train_bpe import find_chunk_boundaries
+import multiprocessing
 
 def preprocess(data_path: str | os.PathLike | typing.BinaryIO | typing.IO[bytes], output_path: str | os.PathLike | typing.BinaryIO | typing.IO[bytes]):
     tokenizer = Tokenizer.from_files("vocab.json", "merges.json", ["<|endoftext|>"])
-    chunk_size = 100_000_000 # 100_000_000 tokens
-    with open(data_path, "r", encoding="utf-8") as f:
-        total_lines = sum(1 for _ in f)
-    with open(data_path, "r", encoding="utf-8") as fin, open(output_path, 'wb') as fout:
-        tokens_buffer = []
-        fin_with_progress = tqdm(fin, total=total_lines, desc="Encoding lines")
-        for tok in tokenizer.encode_iterable(fin_with_progress):
-            tokens_buffer.append(tok)
-            if len(tokens_buffer) >= chunk_size:
-                arr = np.array(tokens_buffer, dtype=np.uint16)
-                fout.write(arr.tobytes())
-                tokens_buffer.clear()
-        
-        if len(tokens_buffer) > 0:
-            arr = np.array(tokens_buffer, dtype=np.uint16)
+    num_processes = os.cpu_count() or 4
+    with open(data_path, "rb") as f:
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+    worker_args = [(data_path, start, end, tokenizer) for (start, end) in zip(boundaries[:-1], boundaries[1:])]
+    with multiprocessing.Pool(processes=num_processes) as pool:
+        results = pool.starmap(encode_worker, worker_args)
+    with open(output_path, "wb") as fout:
+        for result in results:
+            arr = np.array(result, dtype=np.uint16)
             fout.write(arr.tobytes())
-        tokens_buffer.clear()
+            fout.flush()
+
+
+def encode_worker(input_path: str, start: int, end: int, tokenizer: Tokenizer):
+    with open(input_path, "rb") as f:
+        f.seek(start)
+        chunk_bytes = f.read(end - start).decode("utf-8", errors="ignore")
+        text = chunk_bytes.decode('utf-8')
+    tokens = tokenizer.encode(text)
+    return tokens
+
         
